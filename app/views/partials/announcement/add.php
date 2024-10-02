2894d2a82af600be2e98b0a29f11800a17a35819
unknown <Jayvhonserrano@gmail.com>
2024-10-02 11:37:37 +0800

    initial commit

---
+++ app/views/partials/announcement/add.php
@@ -0,0 +1,110 @@
+<?php
+$comp_model = new SharedController;
+$page_element_id = "add-page-" . random_str();
+$current_page = $this->set_current_page_link();
+$csrf_token = Csrf::$token;
+$show_header = $this->show_header;
+$view_title = $this->view_title;
+$redirect_to = $this->redirect_to;
+?>
+<section class="page" id="<?php echo $page_element_id; ?>" data-page-type="add"  data-display-type="" data-page-url="<?php print_link($current_page); ?>">
+    <?php
+    if( $show_header == true ){
+    ?>
+    <div  class="bg-light p-3 mb-3">
+        <div class="container">
+            <div class="row ">
+                <div class="col "  >
+                    <h4 class="record-title" style="color: black; font-size: 20px;">Add New Announcement</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+    }
+    ?>
+    <div  class="">
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-7 comp-grid">
+                    <?php $this :: display_page_errors(); ?>
+                    <div  class="bg-light p-3 animated fadeIn page-content">
+                        <form id="announcement-add-form" role="form" novalidate enctype="multipart/form-data" class="form page-form form-horizontal needs-validation" action="<?php print_link("announcement/add?csrf_token=$csrf_token") ?>" method="post">
+                            <div>
+                                <div class="form-group ">
+                                    <div class="row">
+                                        <div class="col-sm-4">
+                                            <label class="control-label" for="Announcement" style="color: black; font-size: 20px;">Announcement <span class="text-danger">*</span></label>
+                                        </div>
+                                        <div class="col-sm-8">
+                                            <div class="">
+                                                <textarea placeholder="Enter Announcement" id="ctrl-Announcement"  required="" rows="5" name="Announcement" class=" form-control"><?php  echo $this->set_field_value('Announcement',""); ?></textarea>
+                                                <!--<div class="invalid-feedback animated bounceIn text-center">Please enter text</div>-->
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group ">
+                                    <div class="row">
+                                        <div class="col-sm-4">
+                                            <label class="control-label" for="Description" style="color: black; font-size: 20px;">Description <span class="text-danger">*</span></label>
+                                        </div>
+                                        <div class="col-sm-8">
+                                            <div class="">
+                                                <textarea placeholder="Enter Description" id="ctrl-Description"  required="" rows="5" name="Description" class=" form-control"><?php  echo $this->set_field_value('Description',""); ?></textarea>
+                                                <!--<div class="invalid-feedback animated bounceIn text-center">Please enter text</div>-->
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group ">
+                                    <div class="row">
+                                        <div class="col-sm-4">
+                                            <label class="control-label" for="Date_from" style="color: black; font-size: 20px;">Date From <span class="text-danger">*</span></label>
+                                        </div>
+                                        <div class="col-sm-8">
+                                            <div class="input-group">
+                                                <input id="ctrl-Date_from" class="form-control datepicker  datepicker"  required="" value="<?php  echo $this->set_field_value('Date_from',date_now()); ?>" type="datetime" name="Date_from" placeholder="Enter Date From" data-enable-time="false" data-min-date="" data-max-date="" data-date-format="Y-m-d" data-alt-format="F j, Y" data-inline="false" data-no-calendar="false" data-mode="single" />
+                                                    <div class="input-group-append">
+                                                        <span class="input-group-text"><i class="material-icons">date_range</i></span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group ">
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <label class="control-label" for="Date_to" style="color: black; font-size: 20px;">Date To <span class="text-danger">*</span></label>
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <div class="input-group">
+                                                    <input id="ctrl-Date_to" class="form-control datepicker  datepicker"  required="" value="<?php  echo $this->set_field_value('Date_to',date_now()); ?>" type="datetime" name="Date_to" placeholder="Enter Date To" data-enable-time="false" data-min-date="" data-max-date="" data-date-format="Y-m-d" data-alt-format="F j, Y" data-inline="false" data-no-calendar="false" data-mode="single" />
+                                                        <div class="input-group-append">
+                                                            <span class="input-group-text"><i class="material-icons">date_range</i></span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group form-submit-btn-holder text-center mt-3">
+                                        <div class="form-ajax-status"></div>
+                                        <button class="btn btn-primary" type="submit" style="position: absolute; left: 83%;background-color: #2077c6;border:#8c8c8c solid 1px">
+                                            Save
+                                           
+                                        </button>
+                                     <a href="announcement">  <button class="btn btn-danger" type="button" style="position: absolute; left: 90%">
+                                          Close
+                                            
+                                        </button>
+                                        </a>
+                                    </div>
+                                    
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
